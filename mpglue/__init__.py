f591d5aab78117ecc5ca3ec7f37ec115556dc644
Jordan <Dill@31-59-41-155-wireless1x.bu.edu>
2016-10-19 10:45:04 -0400

    modified:   mpglue/__init__.py 	new file:   mpglue/veg_indices.py

--- mpglue/__init__.py
+++ mpglue/__init__.py
@@ -1,10 +1,13 @@
 from .raster_tools import rinfo, mparray, create_raster, write2raster, batch_manage_overviews, pixel_stats
+
 from .vector_tools import vinfo, create_vector, copy_vector, delete_vector, select_and_save, list_field_names, \
     buffer_vector, add_fields, rename_vector, merge_vectors
+
+from .veg_indices import VegIndicesEquations
 
 
 __all__ = ['__version__', 'rinfo', 'mparray', 'create_raster', 'write2raster', 'batch_manage_overviews', 'vinfo',
            'create_vector', 'copy_vector', 'delete_vector', 'select_and_save', 'list_field_names', 'buffer_vector',
-           'pixel_stats', 'add_fields', 'rename_vector', 'merge_vectors']
+           'pixel_stats', 'add_fields', 'rename_vector', 'merge_vectors', 'VegIndicesEquations']
 
 __version__ = '0.0.1'
