9c7d5e03bf3baff22b73fbe6e5f93fe7008607da
Jordan <graesser@bu.edu>
2017-05-15 17:55:09 -0400

    modified:   mpglue/classification/classification.py

--- mpglue/classification/classification.py
+++ mpglue/classification/classification.py
@@ -490,7 +490,7 @@ class Samples(object):
     A class to handle data samples
 
     Args:
-        file_name (str): Input .txt file with samples and labels.
+        file_name (str or 2d array): Input text file or 2d array with samples and labels.
         perc_samp (Optional[float]): Percent to sample from all samples. Default is .9. *This parameter
             samples from the entire set of samples, regardless of which class they are in.
         perc_samp_each (Optional[float]): Percent to sample from each class. Default is 0. *This parameter
@@ -500,9 +500,9 @@ class Samples(object):
             Example:
                 Sample by percentage = {1:.9, 2:.9, 3:.5}
                 Sample by integer = {1:300, 2:300, 3:150}
-        norm_struct (Optional[bool]): Whether the structure of the data is normals Default is True. In MapPy's
-            case, normal is (X,Y,Var1,Var2,Var3,Var4,...,VarN,Labels), whereas the alternative (i.e., False) is
-            (Labels,Var1,Var2,Var3,Var4,...,VarN)
+        norm_struct (Optional[bool]): Whether the structure of the data is normal. Default is True. 
+            In MapPy's case, normal is (X,Y,Var1,Var2,Var3,Var4,...,VarN,Labels), 
+            whereas the alternative (i.e., False) is (Labels,Var1,Var2,Var3,Var4,...,VarN)
         labs_type (Optional[str]): Read class labels as integer ('int') or float ('float'). Default is 'int'.
         recode_dict (Optional[dict]): Dictionary of classes to recode. Default is {}, or empty dictionary.
         classes2remove (Optional[list]): List of classes to remove from samples. Default is [], or keep
@@ -555,11 +555,20 @@ class Samples(object):
         self.classes2remove = classes2remove
         self.sample_weight = sample_weight
 
-        if not isinstance(self.file_name, str):
-            raise TypeError('The samples file must be a text file.')
+        if isinstance(self.file_name, str):
 
-        # Open the data samples.
-        df = pd.read_csv(self.file_name, sep=',')
+            # Open the data samples.
+            df = pd.read_csv(self.file_name, sep=',')
+
+        elif isinstance(self.file_name, np.ndarray):
+
+            if len(self.file_name.shape) != 2:
+                raise TypeError('The samples array must be a 2d array.')
+
+            df = pd.DataFrame(self.file_name)
+
+        else:
+            raise TypeError('The samples file must be a text file or a 2d array.')
 
         # Parse the headers.
         self.headers = df.columns.values.tolist()
