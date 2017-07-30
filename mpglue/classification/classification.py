4b191f97246362b24741b0817e8d8b80f8a0d16d
jgrss <graesser@bu.edu>
2017-07-30 14:09:03 -0400

    modified:   mpglue/classification/classification.py

--- mpglue/classification/classification.py
+++ mpglue/classification/classification.py
@@ -3598,13 +3598,13 @@ class classification(Samples, EndMembers, Visualization, Preprocessing):
 
                     if self.classifier_info_['n_jobs'] == 1:
 
-                        self.model = ssvm.FrankWolfeSSVM(GridCRF(inference_method='ogm',
+                        self.model = ssvm.FrankWolfeSSVM(GridCRF(inference_method='ad3',
                                                                  neighborhood=4),
                                                          **self.classifier_info_)
 
                     else:
 
-                        self.model = ssvm.OneSlackSSVM(GridCRF(inference_method='ogm',
+                        self.model = ssvm.OneSlackSSVM(GridCRF(inference_method='ad3',
                                                                neighborhood=4),
                                                        **self.classifier_info_)
 
