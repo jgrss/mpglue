dff9eba99d2a2bf4da9ee5bc09bf9d488885fc72
Jordan <graesser@bu.edu>
2018-04-20 16:24:16 -0400

    updates

--- mpglue/vector_tools.py
+++ mpglue/vector_tools.py
@@ -1314,7 +1314,7 @@ class RTreeManager(object):
                                                           top=en[3],
                                                           right=en[1],
                                                           bottom=en[2]),
-                                              utm='-999')
+                                              utm='1')
 
             del bdy_info, bdy_feature, bdy_geometry
 
